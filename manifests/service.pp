1b645c22a1e33767a872ee588acc4398c527a194
Jordi Prats <jordi.prats@gmail.com>
2019-05-06 11:50:39 +0200

    lint

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,12 +1,5 @@
 class sslh::service inherits sslh {
 
-  #
-  validate_bool($sslh::manage_docker_service)
-  validate_bool($sslh::manage_service)
-  validate_bool($sslh::service_enable)
-
-  validate_re($sslh::service_ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${sslh::service_ensure}")
-
   $is_docker_container_var=getvar('::eyp_docker_iscontainer')
   $is_docker_container=str2bool($is_docker_container_var)
 
