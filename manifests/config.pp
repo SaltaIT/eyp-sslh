fbc2d5e3c936783a7ce0fa18a16a85b4496f1083
Jordi Prats <jordi.prats@gmail.com>
2019-05-06 11:28:22 +0200

    lint

--- manifests/config.pp
+++ manifests/config.pp
@@ -23,27 +23,27 @@ class sslh::config inherits sslh {
     mode   => '0640',
   }
 
-  concat::fragment{ "/etc/sslh.cfg base":
+  concat::fragment{ '/etc/sslh.cfg base':
     target  => '/etc/sslh.cfg',
     order   => '0000',
     content => template("${module_name}/sslh-base.erb"),
   }
 
   # listen
-  concat::fragment{ "/etc/sslh.cfg listen base":
+  concat::fragment{ '/etc/sslh.cfg listen base':
     target  => '/etc/sslh.cfg',
     order   => '0100',
     content => template("${module_name}/sslh-listen.erb"),
   }
 
-  concat::fragment{ "/etc/sslh.cfg listen end":
+  concat::fragment{ '/etc/sslh.cfg listen end':
     target  => '/etc/sslh.cfg',
     order   => '0199',
     content => ");\n",
   }
 
   #protocols
-  concat::fragment{ "/etc/sslh.cfg protocols base":
+  concat::fragment{ '/etc/sslh.cfg protocols base':
     target  => '/etc/sslh.cfg',
     order   => '0200',
     content => "\nprotocols:\n(\n",
@@ -51,7 +51,7 @@ class sslh::config inherits sslh {
 
   ## 03 per sslh::protocol
 
-  concat::fragment{ "/etc/sslh.cfg protocols end":
+  concat::fragment{ '/etc/sslh.cfg protocols end':
     target  => '/etc/sslh.cfg',
     order   => '0499',
     content => template("${module_name}/sslh-protocols.erb"),
