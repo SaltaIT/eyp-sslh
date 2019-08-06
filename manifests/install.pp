a9a1767491b6eb86115c69a324ce2430ba590f05
Jordi Prats <jordi.prats@gmail.com>
2019-08-06 09:49:00 +0200

    conf2struct dependencia

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,19 +6,31 @@ class sslh::install inherits sslh {
 
   if($sslh::manage_package)
   {
+    # dependencies
+    package { $sslh::params::install_dependencies:
+      ensure => $sslh::package_ensure,
+    }
+
     exec { 'eyp-sslh which git':
       command => 'which git',
       unless  => 'which git',
     }
 
-    gitclone { 'https://github.com/yrutschle/sslh':
-      creates => "${sslh::srcdir}/sslh",
+    gitclone { 'https://github.com/yrutschle/conf2struct':
+      creates => "${sslh::srcdir}/conf2struct",
       require => Exec['eyp-sslh which git'],
     }
 
-    #install_dependencies
-    package { $sslh::params::install_dependencies:
-      ensure => $sslh::package_ensure,
+    exec { 'conf2struct make install':
+      command => 'make install',
+      # creates => '/usr/sbin/sslh',
+      cwd     => "${sslh::srcdir}/sslh",
+      require => [ Gitclone['https://github.com/yrutschle/conf2struct'], Package[$sslh::params::install_dependencies] ],
+    }
+
+    gitclone { 'https://github.com/yrutschle/sslh':
+      creates => "${sslh::srcdir}/sslh",
+      require => Exec['eyp-sslh which git'],
     }
 
     # systemd socket activation
